46a47673ace7c4752fdc8b08f8e04609e903d454
Refilwe M Mashile <Refilwe-M-Mashile@users.noreply.github.com>
2021-10-13 21:55:05 +0200

    Inserted Admin Data.

---
+++ db/insertData.sql
@@ -0,0 +1,28 @@
+INSERT INTO
+    Users(
+        Cart,
+        FirstName,
+        LastName,
+        Education,
+        Address,
+        Phone,
+        Email,
+        City,
+        Province,
+        Zip,
+        Password
+    )
+VALUES
+    (
+        0,
+        'Refilwe',
+        'Mashile',
+        'Graduate',
+        'Varstity RD',
+        084789657,
+        'admin@mail.com',
+        'Pretoria',
+        'Gauteng',
+        2285,
+        'Admin1'
+    );
