8cb81f6ad9bec1f09e8f250cfeaf6eb57e357646
Refilwe M Mashile <Refilwe-M-Mashile@users.noreply.github.com>
2021-10-13 21:10:09 +0200

    Added Database

--- index.js
+++ index.js
@@ -11,3 +11,32 @@ navToggle.addEventListener("click", function () {
     this.setAttribute("aria-expanded", "true");
   }
 });
+
+//------------------------ Register ----------------------
+function register(){
+  
+}
+
+//------------------------ For Users ----------------------
+const fs = require('fs');
+class User{
+  constructor(firstName, lastName, email, education, address, city, province, zip, password){
+    this.cart = [];
+    this.items = this.cart.length;
+    this.UserData = {'firstName':firstName,'lastName':lastName,'email':email,'education':education,'address':address,'city': city,"province":province,"zip":zip,"password":password}
+}
+  addCart(index){
+    this.cart.push(tutor[index]);
+    this.items = this.cart.length;
+  }
+
+  getItems(){
+    return this.items;
+  }
+
+  getCart(){
+    this.cart;
+  }
+
+}
+
